126443842b2d986d3c0c4b06e569f43dda5a36fa
Markus Nam <chnam@student.ubc.ca>
2023-03-14 17:18:24 -0700

    add library name in front of function call; comment out unused library

--- app.R
+++ app.R
@@ -1,12 +1,12 @@
 library(shiny)
 library(ggplot2)
-library(bslib)
+#library(bslib)
 library(thematic)
 library(plotly)
 library(tidyverse)
 library(shinyWidgets)
 library(leaflet)
-library(rgdal)
+#library(rgdal)
 library(shinydashboard)
 
 # Optimizing workflow
@@ -18,9 +18,9 @@ df <- read.csv(file = 'data/raw/crimedata_csv_AllNeighbourhoods_AllYears.csv')
 # Data wrangling
 df[df == ''] <- NA
 df <- df |>
-  drop_na() |>  # Drop NA
-  filter(YEAR < max(df$YEAR))  |> # Remove the latest year data (not full year in general) 
-  filter(X != 0 & Y != 0)
+  tidyr::drop_na() |>  # Drop NA
+  dplyr::filter(YEAR < max(df$YEAR))  |> # Remove the latest year data (not full year in general) 
+  dplyr::filter(X != 0 & Y != 0)
 
 # Get year range
 year_range <- range(df$YEAR)
@@ -32,18 +32,18 @@ unique_crimetype <- sort(unique(df$TYPE))
 # prepare UTM coordinates matrix
 # PS: df$X and df$Y are UTM Easting and Northing respectively
 # PS: zone= UTM zone, vancouver is UTM10
-utmcoor <- SpatialPoints(cbind(df$X, df$Y), proj4string = CRS("+proj=utm +zone=10"))
+utmcoor <- sp::SpatialPoints(cbind(df$X, df$Y), proj4string = sp::CRS("+proj=utm +zone=10"))
 
 #From utm to latitude or longtitude
-longlatcoor <- data.frame(spTransform(utmcoor, CRS("+proj=longlat")))
+longlatcoor <- data.frame(sp::spTransform(utmcoor, sp::CRS("+proj=longlat")))
 colnames(longlatcoor) <- c("Longtitude", "Latitude")
 df <- cbind(df, longlatcoor)
 
 # UI
-ui <- dashboardPage(
-  dashboardHeader(title = "Vancouver Crime Data"),
-  dashboardSidebar(
-    sliderInput(
+ui <- shinydashboard::dashboardPage(
+  shinydashboard::dashboardHeader(title = "Vancouver Crime Data"),
+  shinydashboard::dashboardSidebar(
+    shiny::sliderInput(
       inputId = "year",
       label = "YEAR:",
       min = year_range[1],
@@ -52,7 +52,7 @@ ui <- dashboardPage(
       step = 1,
       sep = ''
     ),
-    pickerInput(
+    shinyWidgets::pickerInput(
       inputId = "nhood",
       label = "Select Vancouver Neighbourhood",
       choices = unique_nhood,
@@ -64,7 +64,7 @@ ui <- dashboardPage(
       ),
       multiple = TRUE
     ),
-    pickerInput(
+    shinyWidgets::pickerInput(
       inputId = "crimetype",
       label = "Select Types of Crimes",
       choices = unique_crimetype,
@@ -77,25 +77,25 @@ ui <- dashboardPage(
       multiple = TRUE
     )
   ),
-  dashboardBody(
-    tabsetPanel(
+  shinydashboard::dashboardBody(
+    shiny::tabsetPanel(
       # First tab
-      tabPanel(
+      shiny::tabPanel(
         "Number of crimes",
-        fluidRow(
-          box(
+        shiny::fluidRow(
+          shinydashboard::box(
             title = "Average Number of Crimes by Time",
             plotlyOutput(outputId = 'crime_hour_plot'),
             width = 6
           ),
-          box(
+          shinydashboard::box(
             title = "Trend of total number of crimes",
             plotlyOutput(outputId = 'crime_neighbourhood_plot'),
             width = 6
           )
         ),
-        fluidRow(
-          box(
+        shiny::fluidRow(
+          shinydashboard::box(
             title = "Number of crimes by type",
             plotlyOutput(outputId = 'crime_type_plot'),
             width = 12
@@ -103,10 +103,10 @@ ui <- dashboardPage(
         )
       ),
       # Second tab for map
-      tabPanel(
+      shiny::tabPanel(
         "Crime Map",
-        fluidRow(
-          box(
+        shiny::fluidRow(
+          shinydashboard::box(
             title = "Crime Map",
             leafletOutput("CrimeMap", height = "600px"),
             width = 12
@@ -114,35 +114,34 @@ ui <- dashboardPage(
         )
       ),
       # Third tab for learn more
-      tabPanel(
+      shiny::tabPanel(
         "Learn More",
-        box(
+        shinydashboard::box(
           HTML("<p>The current project is a dahsbord  for the Vancouver (Canada) crime data released by the Vancouver Police Department (<a href='https://vpd.ca/'>VPD</a>).</p>"),
           HTML("<p>For more information, please visit our GitHub repository at <a href='https://github.com/UBC-MDS/VanCrime'>https://github.com/UBC-MDS/VanCrime</a>.</p>"),
           h4('License'),
           p('The current project was created by Morris Chan, Markus Nam, Andy Wang and Tony Zoght. It is licensed under the terms of the MIT license.'),
           width = 12
         )
-        
       )
     )
   )
 )
 
 server <- function(input, output, session) {
-  df_select <- reactive({
+  df_select <- shiny::reactive({
     df |>
-      filter(YEAR >= input$year[1],
+      dplyr::filter(YEAR >= input$year[1],
              YEAR <= input$year[2],
              NEIGHBOURHOOD %in% input$nhood,
              TYPE %in% input$crimetype) |>
-      add_count(TYPE)
+      dplyr::add_count(TYPE)
   })
   
   # Plot - Total Crimes by Type
-  output$crime_type_plot <- renderPlotly({
+  output$crime_type_plot <- plotly::renderPlotly({
     if (nrow(df_select()) == 0) {
-      ggplot() + 
+      ggplot2::ggplot() + 
         annotate("text", x = 0.5, y = 0.5, label = "No values selected",
                  size = 8, color = "red", hjust = 0.5, vjust = 0.5) +
         labs(x='Count',
@@ -156,8 +155,8 @@ server <- function(input, output, session) {
               axis.text.y=element_text(family="Arial"))
     }
     else {
-      ggplotly(
-        ggplot(df_select(), aes(y=reorder(TYPE, -n), fill=TYPE, text=paste0('count: ', n))) +
+      plotly::ggplotly(
+        ggplot2::ggplot(df_select(), aes(y=reorder(TYPE, -n), fill=TYPE, text=paste0('count: ', n))) +
           geom_bar(stat='count') +
           labs(x='Count',
                y='Crime type') +
@@ -175,10 +174,10 @@ server <- function(input, output, session) {
   })
 
   # Plot - Crime Map
-  output$CrimeMap <- renderLeaflet({
-    leaflet(df_select()) %>%
-      addTiles() %>%
-      addMarkers(
+  output$CrimeMap <- leaflet::renderLeaflet({
+    leaflet::leaflet(df_select()) %>%
+      leaflet::addTiles() %>%
+      leaflet::addMarkers(
         lng = ~ Longtitude,
         lat = ~ Latitude,
         label = ~ paste0(TYPE, " (", HOUR, ":", MINUTE, ")"),
@@ -187,9 +186,9 @@ server <- function(input, output, session) {
   })
   
   # Plot - Total Crimes by Neighbourhood
-  output$crime_neighbourhood_plot <- renderPlotly({
+  output$crime_neighbourhood_plot <- plotly::renderPlotly({
     if (nrow(df_select()) == 0) {
-      ggplot() + 
+      ggplot2::ggplot() + 
         annotate("text", x = 0.5, y = 0.5, label = "No values selected",
                  size = 8, color = "red", hjust = 0.5, vjust = 0.5) +
         labs(x='Year',
@@ -204,7 +203,7 @@ server <- function(input, output, session) {
               axis.text.y=element_text(family="Arial"))
     }
     else if (length(unique(df_select()$YEAR)) <= 1) {
-      ggplot() + 
+      ggplot2::ggplot() + 
         annotate("text", x = 0.5, y = 0.5,
                  label = "Please select at least two years.",
                  size = 8, color = "red", hjust = 0.5, vjust = 0.5) +
@@ -220,8 +219,8 @@ server <- function(input, output, session) {
               axis.text.y=element_text(family="Arial"))
     }
     else {
-      ggplotly(
-        ggplot(df_select(), aes(x=YEAR, fill=NEIGHBOURHOOD)) +
+      plotly::ggplotly(
+        ggplot2::ggplot(df_select(), aes(x=YEAR, fill=NEIGHBOURHOOD)) +
           geom_area(stat='count') +
           labs(x='Year',
                y='Total number of crimes',
@@ -238,9 +237,9 @@ server <- function(input, output, session) {
   })
 
   # Plot - Total Crimes by Hour
-  output$crime_hour_plot <- renderPlotly({
+  output$crime_hour_plot <- plotly::renderPlotly({
     if (nrow(df_select()) == 0) {
-      ggplot() + 
+      ggplot2::ggplot() + 
         annotate("text", x = 0.5, y = 0.5, label = "No values selected",
                  size = 8, color = "red", hjust = 0.5, vjust = 0.5) +
         labs(x='Time',
@@ -255,12 +254,12 @@ server <- function(input, output, session) {
     }
     else {
       df_group <- df_select() |>
-        group_by(HOUR) |>
-        summarise(n = n()) |>
-        mutate(Time = as.factor(paste(HOUR, ':00', sep = '')),
+        dplyr::group_by(HOUR) |>
+        dplyr::summarise(n = n()) |>
+        dplyr::mutate(Time = as.factor(paste(HOUR, ':00', sep = '')),
                daily_avg = n / (365 * (input$year[2] - input$year[1] + 1)))
       
-      df_group$Time = fct_relevel(df_group$Time,
+      df_group$Time = forcats::fct_relevel(df_group$Time,
                              c('0:00', '1:00', '2:00', '3:00', '4:00',
                                '5:00', '6:00', '7:00', '8:00', '9:00',
                                '10:00', '11:00', '12:00', '13:00', '14:00',
@@ -268,8 +267,8 @@ server <- function(input, output, session) {
                                '20:00', '21:00', '22:00', '23:00')
       )
       
-      ggplotly(
-        ggplot(df_group, aes(x=Time, y = daily_avg, group = 1)) +
+      plotly::ggplotly(
+        ggplot2::ggplot(df_group, aes(x=Time, y = daily_avg, group = 1)) +
           geom_line() +
           labs(x='Time',
                y='Daily Average') +
@@ -287,5 +286,5 @@ server <- function(input, output, session) {
   })
 }
 
-thematic_shiny()
-shinyApp(ui, server)
+thematic::thematic_shiny()
+shiny::shinyApp(ui, server)
