142d4ccf11d2e7604afdf1f0bc7977a33dd36120
Andy Wang <wang64192664@gmail.com>
2023-03-17 00:59:49 -0800

    removed failing test

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -7,9 +7,3 @@ test_that("{shinytest2} recording: should_return_empty", {
 })
 
 
-
-test_that("{shinytest2} recording: test_correct_plots_1", {
-  app <- AppDriver$new(name = "test_correct_plots_1", height = 811, width = 1014, load_timeout = 100000)
-  app$set_inputs(tab1 = "Number of crimes")
-  app$expect_values()
-})
