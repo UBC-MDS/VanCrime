ec89c94001c90694ed8b2513f343e62da628925c
Andy Wang <wang64192664@gmail.com>
2023-03-17 00:42:47 -0800

    add 2nd tests

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -5,3 +5,10 @@ test_that("{shinytest2} recording: should_return_empty", {
   app$set_inputs(nhood = character(0))
   app$expect_values()
 })
+
+
+test_that("{shinytest2} recording: shouldReturnMultiPlots", {
+  app <- AppDriver$new(name = "shouldReturnMultiPlots", height = 811, width = 1014, load_timeout = 100000)
+  app$set_inputs(tab1 = "Number of crimes")
+  app$expect_values()
+})
