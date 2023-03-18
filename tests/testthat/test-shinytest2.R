6fa26dd672d1890da10c1978ea5023a0478068c6
Andy Wang <wang64192664@gmail.com>
2023-03-17 16:03:45 -0800

    get rid of tests

--- tests/testthat/test-shinytest2.R
+++
@@ -1,7 +0,0 @@
-library(shinytest2)
-
-test_that("{shinytest2} recording: test_tab1_empty_map", {
-  app <- AppDriver$new(name = "test_tab1_empty_map", height = 857, width = 1406, load_timeout = 100000)
-  app$set_inputs(crimetype = character(0))
-  app$expect_values()
-})
