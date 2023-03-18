4beb6f85a819461a87e35914ecde137c2c21caa3
Andy Wang <wang64192664@gmail.com>
2023-03-17 17:41:48 -0800

    add 3rd passing test

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -16,3 +16,11 @@ test_that("{shinytest2} recording: tab3_test_should_pass", {
   app$expect_values()
   app$expect_screenshot()
 })
+
+
+test_that("{shinytest2} recording: tab2_should_pass_with_no_change", {
+  app <- AppDriver$new(name = "tab2_should_pass_with_no_change", height = 857, width = 1406, load_timeout = 100000)
+  app$set_inputs(year = c(2003, 2012))
+  app$set_inputs(nhood = character(0))
+  app$expect_values()
+})
