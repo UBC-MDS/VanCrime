a7c7f9ab77c5788e8782ceb7283c1b25fcc4356f
Andy Wang <wang64192664@gmail.com>
2023-03-17 17:32:40 -0800

    add test2

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -6,3 +6,13 @@ test_that("{shinytest2} recording: tab1_test_should_pass", {
   app$set_inputs(crimetype = character(0))
   app$expect_values()
 })
+
+
+test_that("{shinytest2} recording: tab3_test_should_pass", {
+  app <- AppDriver$new(variant = platform_variant(), name = "tab3_test_should_pass", 
+      height = 857, width = 1406, load_timeout = 100000)
+  app$set_inputs(nhood = character(0))
+  app$set_inputs(tab1 = "Learn More")
+  app$expect_values()
+  app$expect_screenshot()
+})
