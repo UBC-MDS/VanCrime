d3e455d789515a3751124cccb4c364386e1f37f1
Andy Wang <wang64192664@gmail.com>
2023-03-17 16:30:03 -0800

    delete tests

--- tests/testthat/test-shinytest2.R
+++
@@ -1,7 +0,0 @@
-library(shinytest2)
-
-test_that("{shinytest2} recording: test_tab1_with_no_neigh", {
-  app <- AppDriver$new(name = "test_tab1_with_no_neigh", height = 857, width = 1406, load_timeout = 100000)
-  app$set_inputs(nhood = character(0))
-  app$expect_values()
-})
